82cdb3ac92237d19e5ceee7825806abd4a4e87c5
KajeQ <zakialfikri313@gmail.com>
2026-05-04 19:40:04 +0700

    feat: Refactor FoodCard scenes and add detail UI
    Move FoodCardResource.gd to project root and update .tres references accordingly. Rename scenes/card.tscn to scenes/FoodCard.tscn and rename the root node; replace explicit MieGoreng button with an instance of the new FoodCard scene. Add focus tween handlers in food_layout.gd and enable a Detail container in food_layout.tscn. Add a Player.tscn scene and remove legacy card_hand.tscn and card_slot.tscn files. Update scenes/main.gd to extend Node (add a show_card_details stub and some debug comments) and overhaul main.tscn: adjust anchors/layouts, add an arc shape resource, include the FoodCard PackedScene, and add a CardDetailPanel UI with nutritional labels. These changes restructure card resources and scene layout to support improved card interaction and a details UI.

--- food_layout.gd
+++ food_layout.gd
@@ -11,3 +11,8 @@ func _update_display() -> void:
 	title.text = data.card_name
 	image.texture = data.card_image
 	
+func _focus_in() -> void:
+	card_instance.tween_scale(Vector2(1.2,1.2), 0.2)
+
+func _focus_out() -> void:
+	card_instance.tween_scale(Vector2(1,1), 0.2)
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	#delta *= 2
+	#
+	#var frame = 0
+	#frame += 1 * delta
+	#print(frame)
+	#
+	#var fps = 1 / delta
+	#print(fps)
+		
+func show_card_details(Card):
+	pass
+	
