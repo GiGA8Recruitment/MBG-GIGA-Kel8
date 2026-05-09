2080b8d675ab4f687bb93c84fb14d7de4ff9792e
KajeQ <zakialfikri313@gmail.com>
2026-05-09 10:16:23 +0700

    Add food deck, card resources and UI detail logic
    Register food cards with a front layout and add a CardDeck resource, update main scene and script to spawn cards from the new deck and show a detail panel. Changes include: update layout cache timestamps, add front_layout_name to food card .tres files, add cards/food_deck.tres, implement spawn_from_deck and detail display in scenes/main.gd (connects card signals and populates UI), and reorganize scenes/main.tscn to use the CardDeckManager, reference the new deck resource, and restructure the CardDetailPanel and stats UI. Minor scene/node cleanup: removed hardcoded card instances from the scene in favor of dynamic spawning.

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,23 +1,48 @@
 extends Node
 
+@onready var player_hand = $CardHand
+@onready var detail_panel = $UI/CardDetailPanel
+@onready var food_deck: CardDeck
+@onready var food_deck_manager = $FoodCardDeckManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	food_deck = food_deck_manager.deck
+	detail_panel.hide()
+	player_hand.card_added.connect(_on_card_added)
+	spawn_from_deck(food_deck)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	#delta *= 2
-	#
-	#var frame = 0
-	#frame += 1 * delta
-	#print(frame)
-	#
-	#var fps = 1 / delta
-	#print(fps)
+func spawn_from_deck(deck: CardDeck):
+	if not deck:
+		push_error("Data belum di-assign!")
+	
+	for card_resource in deck.get_cards():
+		if player_hand.is_full():
+			print("Hand sudah penuh!")
+			return
 		
-func show_card_details(Card):
-	pass
+		var new_card = Card.new(card_resource)
+		add_child(new_card)
+		new_card.move_to(player_hand)
+	
+func _on_card_added(card: Card, _index: int):
+	card.card_clicked.connect(_show_details)
+	card.drag_started.connect(_show_details)
+		
+func _show_details(card: Card):
+	var data = card.card_data as FoodCardResource
+	
+	if data:
+		detail_panel.get_node("CardDetailContainer/Nama").text = "Nama: " + data.card_name
+		detail_panel.get_node("CardDetailContainer/Harga").text = "Harga: " + str(data.harga)	
+		detail_panel.get_node("CardDetailContainer/Karbohidrat").text = "Karbo: " + str(data.karbohidrat)
+		detail_panel.get_node("CardDetailContainer/Protein").text = "Protein: " + str(data.protein)
+		detail_panel.get_node("CardDetailContainer/Vitamin").text = "Vitamin: " + str(data.vitamin)
+		detail_panel.get_node("CardDetailContainer/Lemak").text = "Lemak: " + str(data.lemak)
+		detail_panel.get_node("CardDetailContainer/Gula").text = "Gula: " + str(data.gula)
+		detail_panel.show()
+
+#func _process(delta: float) -> void:
+	#pass
+	
 	
